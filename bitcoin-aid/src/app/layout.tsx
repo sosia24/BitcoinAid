d9ed8bb134aafa0d30bf4cc3a40ff774dd0677e3
sosia24 <sosia24@proton.me>
2024-09-29 15:21:57 -0300

    New Commit

--- bitcoin-aid/src/app/layout.tsx
+++ bitcoin-aid/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           <Header />
           <main className={franklin.className}>{children}</main>
           <a href="https://t.me/seulink" target="_blank">
-           <div className="fixed md:bottom-[90px] bottom-[80px] right-[2%] lg:right-[5%] bg-blue-500 text-white p-2 rounded-full shadow-lg hover:bg-blue-600 transition transform hover:scale-110">
+           <div className="fixed md:bottom-[90px] bottom-[80px] right-[2%] bg-blue-500 text-white p-2 rounded-full shadow-lg hover:bg-blue-600 transition transform hover:scale-110">
               <FaTelegramPlane className="md:w-6 md:h-6 w-4 h-4" />
            </div>
           </a>
