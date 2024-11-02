970a6f9fa629e369a4ce4fbfd06e9168fe583c2d
sosia24 <sosia24@proton.me>
2024-11-01 22:40:39 -0300

    New Commit

--- bitcoin-aid/src/app/tournament/page.tsx
+++ bitcoin-aid/src/app/tournament/page.tsx
@@ -1,283 +1,52 @@
 "use client";
 import { useEffect, useState } from "react";
 import Image from "next/image";
-import confetti from "canvas-confetti";
-import { FaCrown } from "react-icons/fa6";
-import { PiMedalFill } from "react-icons/pi";
-import { tournamentDonation } from "@/services/Web3Services";
+import confetti from "canvas-confetti"; // Importa a biblioteca de confete
+
 export default function Tournament() {
-
-  
-    const [todayDate, setTodayDate] = useState('');
-    const [dataDonationDaily, setDataDonationDaily] = useState([]);
-    const [dataDonationMonthly, setDataDonationMonthly] = useState([]);
-    const [dataTokenDaily, setDataTokenDaily] = useState([]);
-    const [dataTokenMonthly, setDataTokenMonthly] = useState([]);
-    const [dataNftDaily, setDataNftDaily] = useState([]);
-    const [dataNftMonthly, setDataNftMonthly] = useState([]);
-
-    const [searchDate, setSearchDate] = useState('');
-    const [searchHistorical, setSearchHistorical] = useState([]);
-    const [HistoricalOpen, setHistoricalOpen] = useState(false);
-
-    const [timeDayRemaining, setTimeDayRemaining] = useState(0);
-    const [timeMonthlyRemaining, setTimeMonthlyRemaining] = useState(0);
-
-    const [donationRewards24, setDonationRewards24] = useState(0);
-    const [donationRewards30, setDonationRewards30] = useState(0);
-
-
-    async function rewardsDonation(){
-      try{
-      const {token24, token30} = await tournamentDonation();
-      setDonationRewards24(Number(token24)/1000000)
-      setDonationRewards30(Number(token30)/1000000)
-      console.log(token24)
-      console.log(token30)
-      }catch(err){
-        console.log("Erro")
-      }
-    }
-
-    useEffect(() =>{
-      rewardsDonation();
-    })
-
-
-    useEffect(() => { 
-      const formatDate = (date: Date) => {
-        const year = date.getUTCFullYear();
-        const day = String(date.getUTCDate()).padStart(2, '0');
-        const month = String(date.getUTCMonth() + 1).padStart(2, '0'); // Mês começa em 0
-        return `${year}-${month}-${day}`;
-      };
-    
-      // Definindo a data de hoje no formato correto
-      const today = new Date();
-      const formattedDate = formatDate(today);
-      setTodayDate(formattedDate);
-    
-      // Função para buscar os dados da API
-      const fetchDataDonation = async (max = 3, delay = 1000) => {
-        let attempt = 1;
-        while (attempt <= max) {
-          try {
-            const dataDay = formattedDate; // Use formattedDate
-            const urlDay = `http://67.217.228.116:3000/api/leaderboard_donation?data=${dataDay}`;
-    
-            const currentMonthUTC = String(new Date().getUTCMonth() + 1).padStart(2, '0');
-            const dataMonthly = '2024-11-01'; // Considera que a data é fixa aqui
-            const urlMonthly = `http://67.217.228.116:3000/api/leaderboard_donation_monthly?data=${dataMonthly}`;
-            console.log(urlMonthly);
-    
-            // Faz a requisição e aguarda a resposta
-            const resDay = await fetch(urlDay);
-            const resMonthly = await fetch(urlMonthly);
-    
-            // Verifica se a resposta é bem-sucedida
-            if (!resDay.ok) {
-              throw new Error(`HTTP error! status: ${resDay.status}`);
-            }
-            if (!resMonthly.ok) {
-              throw new Error(`HTTP error! status: ${resMonthly.status}`);
-            }
-    
-            // Processa o resultado JSON
-            const resultDay = await resDay.json();
-            const resultMonthly = await resMonthly.json();
-            setDataDonationDaily(resultDay);
-            setDataDonationMonthly(resultMonthly); // Atualiza o estado com os dados recebidos
-            break; // Se a requisição for bem-sucedida, sai do loop
-          } catch (error) {
-            console.error('Erro ao buscar dados:', error);
-            if (attempt === max) {
-              // Se for a última tentativa, você pode adicionar alguma lógica aqui, como um alerta ao usuário.
-            }
-          }
-          attempt++;
-          await new Promise(resolve => setTimeout(resolve, delay));
-        }
-      };
-    
-      // Chama a função para buscar os dados uma vez ao carregar o componente
-      fetchDataDonation();
-    }, []); // Adicione dependências aqui, se necessário
-    
-
+    const [confettiStarted, setConfettiStarted] = useState(false); // Estado para verificar se o confete já começou
 
     useEffect(() => {
-        // Função que calcula o tempo restante até o final do dia
-        const calculateTimeRemaining = () => {
-          const now = new Date(Date.now());
-          const endOfDay = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(), 23, 59, 59, 999));
-          return endOfDay.getTime() - now.getTime();
-        };
-    
-        // Atualizar o estado imediatamente
-        setTimeDayRemaining(calculateTimeRemaining());
-    
-        // Atualizar o estado a cada milissegundo
-        const interval = setInterval(() => {
-          const remaining = calculateTimeRemaining();
-          if (remaining <= 0) {
-            clearInterval(interval); // Parar o countdown ao atingir zero
-            setTimeDayRemaining(0);
-          } else {
-            setTimeDayRemaining(remaining);
-          }
-        }, 1);
-    
-        // Limpar o intervalo quando o componente for desmontado
-        return () => clearInterval(interval);
-      }, []);
-    
+        if (!confettiStarted) {
+            setConfettiStarted(true); // Marca que os confetes foram iniciados
 
-      const formatTime = (time: number) => {
-        const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((time / (1000 * 60)) % 60);
-        const seconds = Math.floor((time / 1000) % 60);
-    
-        // Formatar para garantir duas casas em horas, minutos e segundos
-        const format = (num: number, digits: number = 2) => String(num).padStart(digits, '0');
-    
-        return `${format(hours)}H:${format(minutes)}M:${format(seconds)}S`;
-      };
+            // Inicia os confetes
+            const startConfetti = () => {
+                confetti({
+                    spread: 70,
+                    startVelocity: 30,
+                    particleCount: 100,
+                    decay: 0.9,
+                    scalar: 1.2,
+                });
+            };
 
+            startConfetti(); // Dispara os confetes
 
-      useEffect(() => {
-        // Função que calcula o tempo restante até o final do mês
-        const calculateTimeRemaining = () => {
-          const now = new Date();
-          const endOfMonth = new Date(Date.UTC(now.getUTCFullYear(), now.getUTCMonth() + 1, 0, 23, 59, 59, 999));
-          return endOfMonth.getTime() - now.getTime();
-        };
-    
-        setTimeMonthlyRemaining(calculateTimeRemaining());
-    
-        // Atualizar o estado a cada segundo
-        const interval = setInterval(() => {
-          const remaining = calculateTimeRemaining();
-          if (remaining <= 0) {
-            clearInterval(interval); // Parar o countdown ao atingir zero
-            setTimeMonthlyRemaining(0);
-          } else {
-            setTimeMonthlyRemaining(remaining);
-          }
-        }, 1000); // Atualização a cada segundo
-    
-        // Limpar o intervalo quando o componente for desmontado
-        return () => clearInterval(interval);
-      }, []);
-    
-      // Converter o tempo restante em dias, horas, minutos e segundos
-      const formatTimeMonthly = (time: number) => {
-        const days = Math.floor(time / (1000 * 60 * 60 * 24));
-        const hours = Math.floor((time / (1000 * 60 * 60)) % 24);
-        const minutes = Math.floor((time / (1000 * 60)) % 60);
-        const seconds = Math.floor((time / 1000) % 60);
-    
-        // Formatar para garantir duas casas em horas, minutos e segundos
-        const format = (num: number, digits: number = 2) => String(num).padStart(digits, '0');
-    
-        return `${days}D ${format(hours)}H ${format(minutes)}M ${format(seconds)}S`;
-      };
+            // Parar os confetes após 3 segundos
+            const stopConfettiTimeout = setTimeout(() => {
+                // Limpa o canvas (não há função stop específica)
+            }, 3000);
+
+            return () => {
+                clearTimeout(stopConfettiTimeout); // Limpa o timeout ao desmontar
+            };
+        }
+    }, [confettiStarted]); // Executa apenas quando confettiStarted muda
 
     return (
-        <>
-        <div className="container min-h-screen max-w-[98%] lg:max-w-[98%] m-auto flex flex-wrap items-center p-[20px] lg:p-[60px]">
-        <div className="w-[100%]">
-            <p className="text-center mt-[30px] text-[26px]">24h Tournaments</p>
-            <div className="md:w-[80%] w-[100%] m-auto items-center justify-center flex md:flex-row flex-col">
-
-
-                <div className="mt-[20px] mr-[15px] podio md:w-[40%] w-[70%] md:text-[18px] text-[14px] glossy p-2 rounded-lg shadow-2xl border-2 border-green-500">
-                    <div className="w-[100%] flex flex-row text-[16px]">
-                        <p className="md:text-[20px] text-[16px] p-[6px]">Donation Daily Tournament</p>
-                    </div>
-                    <div className="w-[100%] p-2">
-                        <div className="flex flex-col items-center mt-[2px]">
-                          {dataDonationDaily.length === 0?(
-                            <p className="text-red-500">No Data</p>
-                          ):(
-                            null
-                          )}
-                            {dataDonationDaily.map((item:any, index) => (
-                                <>
-                                {item.score > 0 && item.rank <= 10?(
-                                  <div className="w-[100%] flex flex-row p-2">
-                                  <p className="" key={index}>{item.rank}° {item.user_address.slice(0,4)}...{item.user_address.slice(-6)}</p>
-                                  {item.rank == 1?(
-                                    <PiMedalFill className="mx-2 text-yellow-400" />
-                                  ):item.rank == 2?(
-                                    <PiMedalFill className="mx-2 text-gray-400" />
-                                  ):item.rank == 3?(
-                                    <PiMedalFill className="mx-2 text-[#804a00]" />
-                                  ):(
-                                    ""
-                                  )}
-                                  <p className="ml-auto">{item.score} $</p>
-                                  </div>
-                                ):(
-                                  ""
-                                )}
-                                
-                                </>
-                           ))}
-                        </div>
-                        <p  className="text-[18px] p-[4px] r-0">Finish in {formatTime(timeDayRemaining)}</p>
-                        <p className="text-[18px] p-[4px] r-0 text-green-700">Actually Rewards: {donationRewards30 ? `${donationRewards30} $` : "Loading..."}</p>
-                    </div>
-                </div>
+        <div className="w-full h-[500px] flex justify-center text-center mt-[120px]">
+            <div className="flex flex-col items-center">
+                <Image 
+                    alt="trophy" 
+                    src={'/images/TrophyComing.png'} 
+                    width={500} 
+                    height={100} 
+                    className="md:max-w-[200px] max-w-[120px] md:max-h-[180px] max-h-[100px]"
+                />
+                <p className="text-[30px]">Coming Soon!</p>
+                <p className="text-[20px]">The biggest tournament BTCA</p>
             </div>
         </div>
-
-
-        <div className="w-[100%]">
-        <p className="text-center mt-[30px] text-[26px]">30-Days Tournaments</p>
-        <div className="w-[80%] m-auto flex flex-col">
-
-            <div className="mt-[20px] m-auto podio lg:w-[80%] w-[100%] lg:text-[20px] text-[16px] glossy p-2 rounded-lg shadow-2xl border-2 border-green-500">
-                <div className="w-[100%] flex flex-row text-[16px]">
-                    <p className="text-[20px] p-[6px]">Donation Monthly Tournament</p>
-                </div>
-                <div className="w-[100%] p-2">
-                    <div className="flex flex-col items-center mt-[2px]">
-                    {dataDonationMonthly.length === 0?(
-                            <p className="text-red-500">No Data</p>
-                          ):(
-                            null
-                          )}
-                    {dataDonationMonthly.map((item:any, index:number) => (
-                                <>
-                                {item.score > 0 && item.rank <= 10?(
-                                  <div className="w-[100%] flex flex-row p-2">
-                                  <p className="" key={index}>{item.rank}° {item.user_address.slice(0,4)}...{item.user_address.slice(-6)}</p>
-                                  {item.rank == 1?(
-                                    <PiMedalFill className="mx-2 text-yellow-400" />
-                                  ):item.rank == 2?(
-                                    <PiMedalFill className="mx-2 text-gray-400" />
-                                  ):item.rank == 3?(
-                                    <PiMedalFill className="mx-2 text-[#804a00]" />
-                                  ):(
-                                    ""
-                                  )}
-                                  <p className="ml-auto">{item.score} $</p>
-                                  </div>
-                                ):(
-                                  ""
-                                )}
-                                
-                                </>
-                           ))}
-                    </div>
-                    <p  className="text-[18px] p-[6px] r-0">Finish in {formatTimeMonthly(timeMonthlyRemaining)}</p>
-                    <p className="text-[18px] p-[4px] r-0 text-green-700">Actually Rewards: {donationRewards30 ? `${donationRewards30} $` : "Loading..."}</p>
-                </div>
-            </div>
-
-            </div>
-        </div>
-        </div>
-    </>
     );
 }
